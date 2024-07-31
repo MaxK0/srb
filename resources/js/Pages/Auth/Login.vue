7bb54e3ebe0777d5a859c1369277ca935e9b117f
MaxK0 <maxkoryakov1@gmail.com>
2024-07-31 21:49:19 +0500

    Сделан логин по номеру или email

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -14,7 +14,7 @@ defineProps({
 });
 
 const form = useForm({
-    email: '',
+    login: '',
     password: '',
     remember: false,
 });
@@ -43,17 +43,17 @@ const submit = () => {
 
         <form @submit.prevent="submit">
             <div>
-                <InputLabel for="email" value="Email" />
+                <InputLabel for="login" value="Email или телефон" />
                 <TextInput
-                    id="email"
-                    v-model="form.email"
-                    type="email"
+                    id="login"
+                    v-model="form.login"
+                    type="text"
                     class="mt-1 block w-full"
                     required
                     autofocus
                     autocomplete="username"
                 />
-                <InputError class="mt-2" :message="form.errors.email" />
+                <InputError class="mt-2" :message="form.errors.login" />
             </div>
 
             <div class="mt-4">
