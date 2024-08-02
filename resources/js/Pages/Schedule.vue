448b73807bbf1039c7899c11578642e6b2992311
MaxK0 <maxkoryakov1@gmail.com>
2024-08-02 19:47:45 +0500

    Улучшения

---
+++ resources/js/Pages/Schedule.vue
@@ -0,0 +1,13 @@
+<script setup>
+import AppLayout from "@/Layouts/AppLayout.vue";
+</script>
+
+<template>
+    <AppLayout title="Расписание">
+        <section class="schedule">
+            <div class="container">
+                fasdfa
+            </div>
+        </section>
+    </AppLayout>
+</template>
