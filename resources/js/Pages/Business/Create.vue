10173869358ca59d0fd7641743cd2e7baa1bdac2
MaxK0 <maxkoryakov1@gmail.com>
2024-09-24 22:15:55 +0500

    Сделана разметка

--- resources/js/Pages/Business/Create.vue
+++ resources/js/Pages/Business/Create.vue
@@ -7,17 +7,17 @@ import InputLabel from "@/Components/InputLabel.vue";
 import InputError from "@/Components/InputError.vue";
 import PrimaryButton from "@/Components/PrimaryButton.vue";
 import TextInput from "@/Components/TextInput.vue";
+import TextArea from "@/Components/TextArea.vue";
 
 import Select from "primevue/select";
 
-const cities = ref([
-    {'title': 'Уфа'}, {'title': 'Москва'}
-]);
-
-const selectedCity = ref("");
+const cities = ref([{ title: "Уфа" }, { title: "Москва" }]);
 
 const form = useForm({
     title: "",
+    city: "",
+    address: "",
+    information: "",
 });
 
 const submit = () => {};
@@ -38,12 +38,13 @@ const submit = () => {};
                             autofocus
                             autocomplete="title"
                         />
+                        <InputError :message="form.errors.title" />
                     </div>
                     <div class="form__blocks">
                         <div class="form__block">
                             <InputLabel for="city" value="Город" />
                             <Select
-                                v-model="selectedCity"
+                                v-model="form.city"
                                 :options="cities"
                                 filter
                                 optionLabel="title"
@@ -51,6 +52,7 @@ const submit = () => {};
                                 class="select"
                             >
                             </Select>
+                            <InputError :message="form.errors.city" />
                         </div>
                         <div class="form__block">
                             <InputLabel for="address" value="Адрес" />
@@ -61,7 +63,17 @@ const submit = () => {};
                                 required
                                 autocomplete="address"
                             />
+                            <InputError :message="form.errors.address" />
                         </div>
+                    </div>
+                    <div class="form__block">
+                        <InputLabel for="information" value="Информация" />
+                        <TextArea
+                            rows="5"
+                            v-model="form.information"
+                            autocomplete="information"
+                        ></TextArea>
+                        <InputError :message="form.errors.information" />
                     </div>
                 </form>
             </div>
