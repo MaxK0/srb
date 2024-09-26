ea8dd258610da166adb2c0e17e455418173ee622
ваше_имя <innusya04@mail.ru>
2024-09-26 15:35:07 +0500

    Создание стилей для таблиц

--- resources/js/Pages/Business/Index.vue
+++ resources/js/Pages/Business/Index.vue
@@ -14,6 +14,25 @@ defineProps({
         <section class="business">
             <div class="container">
                 <Link class="btn-main business__create-link" :href="route('businesses.create')">Создать бизнес</Link>
+                <table class="table business__table">
+                    <thead class="thead">
+                        <tr>
+                            <th>Название</th>
+                            <th>Информация</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody class="tbody">
+                        <tr>
+                            <td>Название</td>
+                            <td>Текст</td>
+                            <div>
+                                <button class="btn-main"></button>
+                                <button class="btn-main"></button>
+                            </div>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </section>
     </AppLayout>
