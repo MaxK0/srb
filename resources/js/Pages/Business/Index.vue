25e65ccc6712f1c1cfe5044818784f969ba19cfe
ваше_имя <innusya04@mail.ru>
2024-09-30 13:17:54 +0500

    Сделан стиль для таблицы

--- resources/js/Pages/Business/Index.vue
+++ resources/js/Pages/Business/Index.vue
@@ -25,11 +25,16 @@ defineProps({
                     <tbody class="tbody">
                         <tr>
                             <td>Название</td>
-                            <td>Текст</td>
-                            <div>
-                                <button class="btn-main"></button>
-                                <button class="btn-main"></button>
-                            </div>
+                            <td class="business__table-inf">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam eius aliquam blanditiis eligendi quis soluta accusamus corporis iste unde molestias nesciunt modi ea repudiandae eaque sunt, aperiam et! Hic, nisi.
+                            Voluptatem, earum dicta facilis consequuntur tempore consectetur laudantium repellendus voluptates vel quisquam itaque odit iure? A obcaecati, omnis magni consequuntur tenetur ratione dolore accusamus at dolor debitis aspernatur quaerat illo.
+                            Vero consequuntur atque accusamus deserunt iure alias facere assumenda velit quo odit ullam voluptatem debitis necessitatibus, quasi, eaque quas repudiandae eos similique mollitia repellendus. Beatae vel voluptates id corrupti tenetur!
+                            Id in tenetur dolore ipsum, reiciendis illum esse obcaecati iusto nesciunt nemo sit dicta officiis nam quo voluptate alias ullam quibusdam numquam. Ab veritatis atque sequi. Dolor amet sit ipsum?</td>
+                            <td>
+                                <div class="table__btns">
+                                    <button class="btn-main">Изменить</button>
+                                    <button class="btn-main delete">Удалить</button>
+                                </div>
+                            </td>
                         </tr>
                     </tbody>
                 </table>
