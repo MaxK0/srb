72ed4613e6ba7d6c91314da397db7cfe62c5ef7c
MaxK0 <maxkoryakov1@gmail.com>
2024-10-02 15:08:27 +0500

    Сделан вывод всех бизнесов

--- resources/js/Pages/Business/Index.vue
+++ resources/js/Pages/Business/Index.vue
@@ -2,19 +2,17 @@
 import AppLayout from "@/Layouts/AppLayout.vue";
 import { Link } from "@inertiajs/vue3";
 
-defineProps({
+const props = defineProps({
     businesses: Object
 });
-
-
 </script>
 
 <template>
     <AppLayout title="Бизнесы">
         <section class="business">
             <div class="container">
-                <Link class="btn-main business__create-link" :href="route('businesses.create')">Создать бизнес</Link>
-                <table class="table business__table">
+                <Link class="btn-main business__create-link" :href="route('businesses.create')">Создать бизнес</Link>                
+                <table v-if="businesses" class="table business__table">
                     <thead class="thead">
                         <tr>
                             <th>Название</th>
@@ -23,18 +21,19 @@ defineProps({
                         </tr>
                     </thead>
                     <tbody class="tbody">
-                        <tr v-for="(business, id) in businesses" :key="id">
+                        <tr v-for="(business, id) in businesses.data" :key="id">
                             <td>{{ business.title }}</td>
                             <td class="business__table-inf">{{ business.information }}</td>
                             <td>
                                 <div class="table__btns">
-                                    <button class="btn-main" :href="route('businesses.edit')">Изменить</button>
+                                    <button class="btn-main" :href="route('businesses.edit', business)">Изменить</button>
                                     <button class="btn-main delete">Удалить</button>
                                 </div>
                             </td>
                         </tr>
                     </tbody>
                 </table>
+                <div v-else>Вы ещё не открыли бизнес</div>
             </div>
         </section>
     </AppLayout>
