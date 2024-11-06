db698489903677c6effe7307687e589d490ef357
MaxK0 <maxkoryakov1@gmail.com>
2024-11-06 13:17:13 +0500

    Убран template #header

--- resources/js/Pages/API/Index.vue
+++ resources/js/Pages/API/Index.vue
@@ -11,11 +11,6 @@ defineProps({
 
 <template>
     <AppLayout title="API Tokens">
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                API Tokens
-            </h2>
-        </template>
 
         <div>
             <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
