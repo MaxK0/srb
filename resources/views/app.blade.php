d6819d5cbc3b15207471cffd9fb6decdb1021602
MaxK0 <maxkoryakov1@gmail.com>
2024-11-14 13:46:59 +0500

    Добавлены meta-теги

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,8 +4,12 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Language" content="ru">
 
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
+    <meta name="description" content="Система управления бизнесом.">
+    <meta name="keywords" content="управление бизнесом, бизнес, система управления, CRM, расписание для бизнеса">
+    <meta name="author" content="Коряков Максим Александрович">
 
     <!-- Scripts -->
     @routes
