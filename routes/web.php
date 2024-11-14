fce5baffefaa158d188cd1bfe7f1d2f26678609b
MaxK0 <maxkoryakov1@gmail.com>
2024-11-14 12:25:23 +0500

    Добавлены политики к роутам

--- routes/web.php
+++ routes/web.php
@@ -58,21 +58,88 @@ Route::middleware([
             ->middleware('can:delete,business');
     });
 
+    Route::prefix('/branches')->name('branches.')->group(function () {
+        Route::get('/create', [BranchController::class, 'create'])->name('create')
+            ->middleware('can:create,\App\Models\Owner\Branch');
+
+        Route::post('', [BranchController::class, 'store'])->name('store')
+            ->middleware('can:create,\App\Models\Owner\Branch');
+
+        Route::get('/{branch}', [BranchController::class, 'show'])->name('show')
+            ->middleware('can:view,branch');
+
+        Route::get('/{branch}/edit', [BranchController::class, 'edit'])->name('edit')
+            ->middleware('can:update,branch');
+
+        Route::put('/{branch}', [BranchController::class, 'update'])->name('update')
+            ->middleware('can:update,branch');
+
+        Route::delete('/{branch}', [BranchController::class, 'destroy'])->name('destroy')
+            ->middleware('can:delete,branch');
+    });
+
+    Route::prefix('/positions')->name('positions.')->group(function () {
+        Route::get('', [PositionController::class, 'index'])->name('index')
+            ->middleware('can:viewAny,\App\Models\Employee\Position');
+
+        Route::get('/create', [PositionController::class, 'create'])->name('create')
+            ->middleware('can:create,\App\Models\Employee\Position');
+
+        Route::post('', [PositionController::class, 'store'])->name('store')
+            ->middleware('can:create,\App\Models\Employee\Position');
+
+        Route::get('/{position}', [PositionController::class, 'show'])->name('show')
+            ->middleware('can:view,position');
+
+        Route::get('/{position}/edit', [PositionController::class, 'edit'])->name('edit')
+            ->middleware('can:update,position');
+
+        Route::put('/{position}', [PositionController::class, 'update'])->name('update')
+            ->middleware('can:update,position');
+
+        Route::delete('/{position}', [PositionController::class, 'destroy'])->name('destroy')
+            ->middleware('can:delete,position');
+    });
+
+    Route::prefix('/employees')->name('employees.')->group(function () {
+        Route::get('', [EmployeeController::class, 'index'])->name('index')
+            ->middleware('can:viewAny,\App\Models\Employee\Employee');
+
+        Route::get('/create', [EmployeeController::class, 'create'])->name('create')
+            ->middleware('can:create,\App\Models\Employee\Employee');
+
+        Route::post('', [EmployeeController::class, 'store'])->name('store')
+            ->middleware('can:create,\App\Models\Employee\Employee');
+
+        Route::get('/{employee}', [EmployeeController::class, 'show'])->name('show')
+            ->middleware('can:view,employee');
+
+        Route::get('/{employee}/edit', [EmployeeController::class, 'edit'])->name('edit')
+            ->middleware('can:update,employee');
+
+        Route::put('/{employee}', [EmployeeController::class, 'update'])->name('update')
+            ->middleware('can:update,employee');
+
+        Route::delete('/{employee}', [EmployeeController::class, 'destroy'])->name('destroy')
+            ->middleware('can:delete,employee');
+
+        Route::get('/{user}/hire', [EmployeeController::class, 'hire'])->name('hire')
+            ->middleware('can:hire,user');
+
+        Route::post('/{user}/hire', [EmployeeController::class, 'hireStore'])->name('hire.store')
+            ->middleware('can:hire,user');
+    });
+
+    Route::name('owner.')->group(function () {
+        Route::post('/becomeOwner', [OwnerController::class, 'becomeOwner'])->name('become')
+            ->middleware('can:create,\App\Models\Owner\Owner');
+    });
 
     Route::middleware(IsOwner::class)->group(function () {
         Route::post('users/find', [UserController::class, 'find'])->name('users.find');
-        Route::resource('branches', BranchController::class)->except('index');
-        Route::resource('positions', PositionController::class);
-
-        Route::resource('employees', EmployeeController::class);
-        Route::get('employees/{user}/hire', [EmployeeController::class, 'hire'])->name('employees.hire');
-        Route::post('employees/{user}/hire', [EmployeeController::class, 'hireStore'])->name('employees.hire.store');
 
         Route::resource('clients', ClientController::class)->except(['edit', 'update', 'delete']);
         Route::post('clients/{user}/attachUser', [ClientController::class, 'attachUserToClients'])->name('clients.attachUser');
     });
 
-    Route::name('owner.')->group(function () {
-        Route::post('/becomeOwner', [OwnerController::class, 'becomeOwner'])->name('become');
-    });
 });
