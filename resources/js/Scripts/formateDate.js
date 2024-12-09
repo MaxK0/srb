39dab6b6c0726cd0d00808d4a9f3cb17ea3b37a0
MaxK0 <maxkoryakov1@gmail.com>
2024-12-09 12:46:21 +0500

    Добавлена функция форматирования времени

--- resources/js/Scripts/formateDate.js
+++ resources/js/Scripts/formateDate.js
@@ -1,5 +1,6 @@
 export const formateDate = (dateString) => {
     const date = new Date(dateString);
+
     const options = {
         year: "numeric",
         month: "2-digit",
@@ -7,6 +8,33 @@ export const formateDate = (dateString) => {
         hour: "2-digit",
         minute: "2-digit",
         second: "2-digit",
+        hour12: false,
     };
-    return date.toLocaleDateString("ru-RU", options);
+
+    const formattedDate = new Intl.DateTimeFormat("ru-RU", options).format(
+        date
+    );
+
+    return formattedDate;
+};
+
+export const formateTime = (timeString) => {
+    const [hours, minutes] = timeString.split(":");
+
+    const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+    const date = new Date();
+    date.setUTCHours(parseInt(hours, 10))
+    date.setUTCMinutes(parseInt(minutes, 10));
+
+    const options = {
+        hour: "2-digit",
+        minute: "2-digit",
+        timeZone: userTimeZone,
+        hour12: false,
+    };
+
+    const formattedTime = date.toLocaleString('ru-RU', options);
+
+    return formattedTime;
 };
