61ce3e267db56cfdc77ad89a2bfa9125df62269c
MaxK0 <maxkoryakov1@gmail.com>
2025-01-27 10:25:06 +0500

    Исправлены сиды

--- database/seeders/ExtraDaySeeder.php
+++ database/seeders/ExtraDaySeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use App\Models\Employee\Workday\ExtraDay;
 use App\Models\Employee\Workday\Workday;
+use App\Models\Owner\Branch;
+use App\Models\Owner\Business;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,7 +17,13 @@ class ExtraDaySeeder extends Seeder
     public function run(): void
     {
         ExtraDay::factory()->create([
-            'workday_id' => Workday::first()->id
+            'workday_id' => Business::first()
+            ->branches()
+            ->first()
+            ->employees()
+            ->first()
+            ->workday
+            ->id
         ]);
     }
 }
--- database/seeders/WorklessDaySeeder.php
+++ database/seeders/WorklessDaySeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\Employee\Workday\WorklessDay;
+use App\Models\Owner\Branch;
+use App\Models\Owner\Business;
 use Illuminate\Database\Seeder;
 
 class WorklessDaySeeder extends Seeder
@@ -13,7 +15,13 @@ class WorklessDaySeeder extends Seeder
     public function run(): void
     {
         WorklessDay::factory()->create([
-            'workday_id' => 2
+            'workday_id' => Business::first()
+            ->branches()
+            ->first()
+            ->employees()
+            ->first()
+            ->workday
+            ->id
         ]);
     }
 }
